d7aae3f990fbbb2dd6a488fe8bba53ed316d05e3
Jake Landers <jake@landersweb.com>
2022-12-30 21:09:24 -0800

    added better footer to puck norris

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,29 +1,94 @@
 import BoundsWrapper from "./boundsWrapper"
 import Image from "./image"
 import Link from "./link"
+import NextLink from "next/link"
+
 
 const Footer = () => {
 
-    return <div className="space-y-8">
-        <footer className="p-4 bg-bg-700 shadow md:px-6 md:py-8">
-            <div className="sm:flex sm:items-center sm:justify-between">
-                <Link props={{
-                    href: "https://sapphirenw.com",
-                    child: <Image props={{
-                        src: "/svg/sapphire_text_light.svg",
-                        alt: "Sapphire NW Logo",
-                        divClass: undefined,
-                        imgClass: "mr-3 h-10"
-                    }} />,
-                    isExternal: true,
-                    className: undefined
-                }} />
-            </div>
-            <hr className="my-6 border-bg-500 sm:mx-auto  lg:my-8" />
-            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://sapphirenw.com/" target="_blank" rel="noopener noreferrer" className="hover:underline">Sapphire™</a>. All Rights Reserved.
-            </span>
-        </footer>
-    </div>
+    return <footer className="bg-bg-700">
+        <div className="md:px-4 py-8">
+            <BoundsWrapper>
+                <div className="grid md:grid-cols-2 gap-4">
+                    <div className="space-y-8 grid place-items-center md:block">
+                        <div className="space-y-2">
+                            <h4 className="text-gray-500">Powered by</h4>
+                            <Image props={{
+                                src: "/svg/xcheck_blue_text_light.svg",
+                                alt: "Crosscheck Sports Logo",
+                                divClass: "",
+                                imgClass: "h-[50px]",
+                            }} />
+                        </div>
+                        <p className="text-gray-400 text-center md:text-left">Puck Norris is powered by the Crosscheck Sports engine, a powerful team management tool that powers scheduling, rosters, statistics, images, merchandise, and more.</p>
+                    </div>
+                    <div className="grid grid-cols-2 place-items-center">
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold uppercase text-white">Features</h2>
+                            <ul className="text-gray-400">
+                                <li className="mb-4">
+                                    <Link props={{
+                                        href: "https://crosschecksports.com/schedule-management/",
+                                        child: <>Scheduling System</>,
+                                        isExternal: true,
+                                        className: "hover:underline"
+                                    }} />
+                                </li>
+                                <li className="mb-4">
+                                    <Link props={{
+                                        href: "https://crosschecksports.com/roster-management/",
+                                        child: <>Roster Management</>,
+                                        isExternal: true,
+                                        className: "hover:underline"
+                                    }} />
+                                </li>
+                                <li className="mb-4">
+                                    <Link props={{
+                                        href: "https://crosschecksports.com/merchandise/",
+                                        child: <>Merchandise</>,
+                                        isExternal: true,
+                                        className: "hover:underline"
+                                    }} />
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold uppercase text-white">Resources</h2>
+                            <ul className="text-gray-400">
+                                <li className="mb-4">
+                                    <Link props={{
+                                        href: "https://crosschecksports.com/support/team-create/",
+                                        child: <>Getting Started</>,
+                                        isExternal: true,
+                                        className: "hover:underline"
+                                    }} />
+                                </li>
+                                <li className="mb-4">
+                                    <Link props={{
+                                        href: "https://crosschecksports.com/support/",
+                                        child: <>Support</>,
+                                        isExternal: true,
+                                        className: "hover:underline"
+                                    }} />
+                                </li>
+                                <li className="mb-4">
+                                    <Link props={{
+                                        href: "https://crosschecksports.com/#download",
+                                        child: <>Download</>,
+                                        isExternal: true,
+                                        className: "hover:underline"
+                                    }} />
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </BoundsWrapper>
+        </div>
+        <div className="bg-bg-600 p-4">
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://sapphirenw.com/" target="_blank" rel="noopener noreferrer" className="hover:underline">Sapphire™</a>. All Rights Reserved.</span>
+        </div>
+    </footer>
 
 }
 
