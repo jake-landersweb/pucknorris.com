742b379c8e5dc4c722c26387576c6d74ba69960e
kevinlanders <klanders@landersweb.com>
2023-08-30 12:09:36 -0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,24 @@
-const About = () => {
-    return <div className="h-[95vh] grid place-items-center">
-        <h3>About Puck Norris </h3>
+import PageHeader from "../../components/pageHeader"
 
-        <div>Welcome to the PuckNorris.com Beer League hockey team website! We were founded in 2012 in Portland, Oregon with one goal in mind: to kick some ice and have a blast doing it. Our team consists of a group of dedicated hockey players who share a passion for the sport and a love for a cold beer after the game. We come from all walks of life, but on the ice, we are one team with one mission. Our slogan is "Blood, Sweat, and Beers". We believe that hockey is not just a game; it's a test of your willpower and determination. It's about working hard, giving it your all, and celebrating your victories with a cold one in hand. Throughout the years, we have forged a strong bond both on and off the ice. We have faced tough opponents, suffered injuries, and even lost a few battles, but through it all, we have never lost our sense of camaraderie and friendship. We're a team that supports each other both on and off the ice. On this website, you'll find everything you need to know about our team. From our schedule to our stats, and even some exclusive behind-the-scenes content, you can follow us throughout our journey, both on and off the ice. We are proud to have been a part of the beer league hockey community for almost a decade now, and we are excited to see what the future holds. Join us as we continue to hit the ice, play our hearts out, and crush beers with our brothers in arms!
+const About = () => {
+    return <div className="">
+        <PageHeader className="pb-4">
+        <h3 className="text-2xl text-gray-400 hover:text-white transition-all hover:underline">About Puck Norris</h3>
+            <p className="max-w-2xl text-gray-500 text-center text-xl">Welcome to the PuckNorris.com Beer League hockey team website! We were founded in 2012 in Portland, Oregon with one goal in mind: to kick some ice and have a blast doing it. </p>
+        </PageHeader>
+        
+        
+        <div className="pb-4">Our team consists of a group of dedicated hockey players who share a passion for the sport and a love for a cold beer after the game. We come from all walks of life, but on the ice, we are one team with one mission. Our slogan is "Blood, Sweat, and Beers". We believe that hockey is not just a game; it's a test of your willpower and determination. It's about working hard, giving it your all, and celebrating your victories with a cold one in hand. Throughout the years, we have forged a strong bond both on and off the ice. We have faced tough opponents, suffered injuries, and even lost a few battles, but through it all, we have never lost our sense of camaraderie and friendship. We're a team that supports each other both on and off the ice. On this website, you'll find everything you need to know about our team. From our schedule to our stats, and even some exclusive behind-the-scenes content, you can follow us throughout our journey, both on and off the ice. We are proud to have been a part of the beer league hockey community for almost a decade now, and we are excited to see what the future holds. Join us as we continue to hit the ice, play our hearts out, and crush beers with our brothers in arms!
             
         </div>
     </div>
+
+
+
+
+
+
+
 }
 
 export default About
